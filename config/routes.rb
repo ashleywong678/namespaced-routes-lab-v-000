016c4b383b051c4e9f8271e58b31a8214075411d
Ashley <ashleywong678@gmail.com>
2019-04-21 16:25:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope '/admin', module: 'admin' do
-    resources :preferences, only: [:index]
+  # scope '/admin', module: 'admin' do
+  #   resources :preferences, only: [:index]
 
 end
